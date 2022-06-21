5e67507832434ecbeacde548c422331aa6e2d72d
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-06-21 19:39:51 +0530

    total value need to fix

--- geektrust.js
+++ geektrust.js
@@ -2,12 +2,13 @@
  * todo code is incomplete
  */
 const fs = require("fs");
-const internal = require("stream");
 const filename = process.argv[2];
+//TODO need to fix total count value
 let total =0;
 let trackInfo = {
     BIKE_REGULAR: {
         vehicle: "BIKE",
+        name:"BIKE_REGULAR",
         NUM: 4,
         COST: 60,
         BOOK: []
@@ -15,6 +16,7 @@ let trackInfo = {
     },
     CAR_REGULAR: {
         vehicle: "CAR",
+        name:"CAR_REGULAR",
         NUM: 3,
         COST: 120,
         BOOK: []
@@ -22,6 +24,7 @@ let trackInfo = {
     },
     CAR_VIP: {
         vehicle: "CAR",
+        name:"CAR_VIP",
         NUM: 1,
         COST: 250,
         BOOK: []
@@ -29,6 +32,7 @@ let trackInfo = {
     },
     SUV_REGULAR: {
         vehicle: "SUV",
+        name:"SUV_REGULAR",
         NUM: 2,
         COST: 200,
         BOOK: []
@@ -36,14 +40,17 @@ let trackInfo = {
     },
     SUV_VIP: {
         vehicle: "SUV",
+        name:"SUV_VIP",
         NUM: 1,
         COST: 300,
         BOOK: []
         , COUNT: 0
     }
-}
+};
+
 let vipCost =0;
 let vehicleList = [];
+let vehicleListExtraTime = [];
 function main(dataInput) {
     var inputLines = dataInput.toString().split("\n")
     for (i = 0; i < inputLines.length; i++) {
@@ -64,23 +71,44 @@ function main(dataInput) {
     }
 }
 const addBooking = (type, number, time) => {
-    let vehicleInfo = trackInfo[type+'_REGULAR'];
-    // vehicleInfo[type]
     let timeSplit = time.split(":")
-    if(timeSplit[0]>=11 && timeSplit[0]<13){
+    if((timeSplit[0]>=11 && timeSplit[0]<13 ) || timeSplit[0]>17){
         console.log('INVALID_ENTRY_TIME');
         return;
     }
-    vehicleInfo.BOOK.push({type, number, time,price:vehicleInfo.COST});
+    let vehicleInfo = trackInfo[type+'_REGULAR'];
+    if(vehicleInfo.name === 'SUV_REGULAR' && vehicleInfo.COUNT==2){
+        vehicleInfo = {}
+        vehicleInfo = trackInfo[type+'_VIP'];
+        
+    }
+    if(vehicleInfo.name === 'SUV_VIP' && vehicleInfo.COUNT==1){
+        console.log('RACETRACK_FULL');
+        return;
+    }
+    if(vehicleInfo.name === 'CAR_REGULAR' && vehicleInfo.COUNT==3){
+        vehicleInfo = {}
+        vehicleInfo = trackInfo[type+'_VIP'];
+        
+    }
+    if(vehicleInfo.name === 'CAR_VIP' && vehicleInfo.COUNT==1){
+        console.log('RACETRACK_FULL');
+        return;
+    }
+    
+    vehicleInfo.BOOK.push({type, number, time,price:vehicleInfo.COST,typeName:vehicleInfo.name});
     let endtime = parseInt(timeSplit[0])+3;
     let newEndTom =endtime +':'+timeSplit[1]
 
-    vehicleList.push({type, number, time,newEndTom,price:vehicleInfo.COST})
-    // if (type+'_VIP' == 'SUV_VIP'){
-    //     vipCost = vipCost + (parseInt(vehicleInfo.COST)*3);
-    // }
-    total =  total + (parseInt(vehicleInfo.COST)*3);
-    vehicleInfo.COUNT +=1
+    vehicleList.push({type, number, time,newEndTom,price:vehicleInfo.COST,typeName:vehicleInfo.name})
+    if (vehicleInfo.name == 'SUV_VIP' || vehicleInfo.name == 'CAR_VIP' ){
+        vipCost = vipCost + (parseInt(vehicleInfo.COST)*3);
+    }
+    else {
+        total =  total + (parseInt(vehicleInfo.COST)*3);
+    }
+    vehicleInfo.COUNT =vehicleInfo.COUNT + 1
+    // console.log('================', vehicleInfo);
     console.log('SUCCESS');
 }
 const addExtra = (number, time) => {
@@ -101,15 +129,32 @@ const addExtra = (number, time) => {
         
     if(parseInt(hourCal) ==0 && parseInt(timeSplit[1])<60){
         total += (50);
+        vehicleListExtraTime.push({...findVehicle,extraCost:50})
     }
+    
     if(parseInt(hourCal) >0){
+        vehicleListExtraTime.push({...findVehicle,extraCost:(50*2)})
         total += (50*2);
     }
 }
     console.log('SUCCESS');
 }
 const printInfo = () => {
-    console.log(total, ' ', vipCost)
+    let vipTotal=0;
+    let Total=0;
+    for(j=0;j<vehicleList.length;j++){
+        if(vehicleList[j] && vehicleList[j].typeName === 'CAR_VIP' || vehicleList[j].typeName === 'SUV_VIP'){
+            vipTotal =vipTotal + (vehicleList[j].price*3)
+        }
+        else{
+            Total =Total + (vehicleList[j].price*3)
+        }
+    }
+    for(k=0;k<vehicleListExtraTime.length;k++){
+        Total =Total + (vehicleListExtraTime[k].extraCost)
+       
+    }
+    console.log(Total, ' ', vipTotal)
 }
 data = fs.readFileSync(process.argv[2]).toString();
 main(data);
